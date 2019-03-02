8b81f92b8ea3aaedb1f401f245924bc6efcdb7a3
Dmitri Kuznetsov <kna_int@mail.ru>
2019-03-02 23:53:37 +0200

    Saving initial block positions

--- Assets/Scripts/Game/Movement/OpenWorld/OpenWorldBlock.cs
+++ Assets/Scripts/Game/Movement/OpenWorld/OpenWorldBlock.cs
@@ -6,6 +6,14 @@ public class OpenWorldBlock : MonoBehaviour
 {
     public OpenWorldBlockLocator Locator;
 
+    [ReadOnly]
+    [Tooltip("Current block position in the universe")]
+    public long BlockX;
+
+    [ReadOnly]
+    [Tooltip("Current block position in the universe")]
+    public long BlockZ;
+
     public void Start()
     {
         //make lines to match the size of the block
--- Assets/Scripts/Game/Movement/OpenWorld/OpenWorldController.cs
+++ Assets/Scripts/Game/Movement/OpenWorld/OpenWorldController.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class OpenWorldController : MonoBehaviour
 {
     //Cached blocks
+   // [ReadOnly]
+    //[Tooltip("Currently loaded and active blocks")]
+    //public GameObject[] BlocksAsGameObjects;
+
     [ReadOnly]
     [Tooltip("Currently loaded and active blocks")]
-    public GameObject[] Blocks;
+    public OpenWorldBlock[] Blocks;
 
     [ReadOnly]
     [Tooltip("Current block position of the center")]
@@ -59,23 +63,51 @@ public class OpenWorldController : MonoBehaviour
 
     private void UpdateBlocks()
     {
-        if( Blocks.Length != 9 )
+        if(Blocks.Length != 9 )
         {
-            //initial load
-            Blocks = new GameObject[9];
+            //initial load - create blocks and fill them up
+            var blocksAsGameObjects = new GameObject[9];
 
             //instantiate initial array of blocks
-            Blocks[0] = Instantiate(OpenWorldBlockPrefab, new Vector3(-BlockSize, 0, -BlockSize), Quaternion.identity, this.gameObject.transform);
-            Blocks[1] = Instantiate(OpenWorldBlockPrefab, new Vector3(0, 0, -BlockSize), Quaternion.identity, this.gameObject.transform);
-            Blocks[2] = Instantiate(OpenWorldBlockPrefab, new Vector3(BlockSize, 0, -BlockSize), Quaternion.identity, this.gameObject.transform);
+            blocksAsGameObjects[0] = Instantiate(OpenWorldBlockPrefab, new Vector3(-BlockSize, 0, -BlockSize), Quaternion.identity, this.gameObject.transform);
+            blocksAsGameObjects[1] = Instantiate(OpenWorldBlockPrefab, new Vector3(0, 0, -BlockSize), Quaternion.identity, this.gameObject.transform);
+            blocksAsGameObjects[2] = Instantiate(OpenWorldBlockPrefab, new Vector3(BlockSize, 0, -BlockSize), Quaternion.identity, this.gameObject.transform);
 
-            Blocks[3] = Instantiate(OpenWorldBlockPrefab, new Vector3(-BlockSize, 0, 0), Quaternion.identity, this.gameObject.transform);
-            Blocks[4] = Instantiate(OpenWorldBlockPrefab, new Vector3(0, 0, 0), Quaternion.identity, this.gameObject.transform);
-            Blocks[5] = Instantiate(OpenWorldBlockPrefab, new Vector3(BlockSize, 0, 0), Quaternion.identity, this.gameObject.transform);
+            blocksAsGameObjects[3] = Instantiate(OpenWorldBlockPrefab, new Vector3(-BlockSize, 0, 0), Quaternion.identity, this.gameObject.transform);
+            blocksAsGameObjects[4] = Instantiate(OpenWorldBlockPrefab, new Vector3(0, 0, 0), Quaternion.identity, this.gameObject.transform);
+            blocksAsGameObjects[5] = Instantiate(OpenWorldBlockPrefab, new Vector3(BlockSize, 0, 0), Quaternion.identity, this.gameObject.transform);
 
-            Blocks[6] = Instantiate(OpenWorldBlockPrefab, new Vector3(-BlockSize, 0, BlockSize), Quaternion.identity, this.gameObject.transform);
-            Blocks[7] = Instantiate(OpenWorldBlockPrefab, new Vector3(0, 0, BlockSize), Quaternion.identity, this.gameObject.transform);
-            Blocks[8] = Instantiate(OpenWorldBlockPrefab, new Vector3(BlockSize, 0, BlockSize), Quaternion.identity, this.gameObject.transform);
+            blocksAsGameObjects[6] = Instantiate(OpenWorldBlockPrefab, new Vector3(-BlockSize, 0, BlockSize), Quaternion.identity, this.gameObject.transform);
+            blocksAsGameObjects[7] = Instantiate(OpenWorldBlockPrefab, new Vector3(0, 0, BlockSize), Quaternion.identity, this.gameObject.transform);
+            blocksAsGameObjects[8] = Instantiate(OpenWorldBlockPrefab, new Vector3(BlockSize, 0, BlockSize), Quaternion.identity, this.gameObject.transform);
+
+            //get instances
+            Blocks = new OpenWorldBlock[9];
+
+            Blocks[0] = blocksAsGameObjects[0].GetComponent<OpenWorldBlock>();
+            Blocks[1] = blocksAsGameObjects[1].GetComponent<OpenWorldBlock>();
+            Blocks[2] = blocksAsGameObjects[2].GetComponent<OpenWorldBlock>();
+
+            Blocks[3] = blocksAsGameObjects[3].GetComponent<OpenWorldBlock>();
+            Blocks[4] = blocksAsGameObjects[4].GetComponent<OpenWorldBlock>();
+            Blocks[5] = blocksAsGameObjects[5].GetComponent<OpenWorldBlock>();
+
+            Blocks[6] = blocksAsGameObjects[6].GetComponent<OpenWorldBlock>();
+            Blocks[7] = blocksAsGameObjects[7].GetComponent<OpenWorldBlock>();
+            Blocks[8] = blocksAsGameObjects[8].GetComponent<OpenWorldBlock>();
+
+            //correct block positions
+            Blocks[0].BlockX = -1; Blocks[0].BlockZ = -1;
+            Blocks[1].BlockX = 0; Blocks[1].BlockZ = -1;
+            Blocks[2].BlockX = 1; Blocks[2].BlockZ = -1;
+
+            Blocks[3].BlockX = -1; Blocks[3].BlockZ = 0;
+            Blocks[4].BlockX = 0; Blocks[4].BlockZ = 0;
+            Blocks[5].BlockX = 1; Blocks[5].BlockZ = 0;
+
+            Blocks[6].BlockX = -1; Blocks[6].BlockZ = 1;
+            Blocks[7].BlockX = 0; Blocks[7].BlockZ = 1;
+            Blocks[8].BlockX = 1; Blocks[8].BlockZ = 1;
         }
         else
         {
@@ -93,6 +125,10 @@ public class OpenWorldController : MonoBehaviour
         {
             //block position changed
             UpdateBlocks();
+
+            //remember current position
+            this.BlockX = BlockX;
+            this.BlockZ = BlockZ;
         }
     }
 }
