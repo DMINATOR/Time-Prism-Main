50d1a34e1248547659509d08373fd986699ddc7b
Dmitri Kuznetsov <kna_int@mail.ru>
2019-03-10 23:06:24 +0200

    Added ability to create overloads and add custom save game settings + editor window

---
+++ Assets/Plugins/Project/ProjectOverloads.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//SAVE GAME overloads
+public partial class SaveGameData : DKAsset
+{
+    public int TestField;
+}
