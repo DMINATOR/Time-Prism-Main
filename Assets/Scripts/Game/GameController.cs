4d9866f931b9c75829eebebdb65942de78d37ef1
Dmitri Kuznetsov <kna_int@mail.ru>
2019-04-18 23:06:51 +0300

    Created new scene for testing reverse and updated debug options

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -44,20 +44,6 @@ public class GameController : MonoBehaviour
         Log.Instance.Info(GameController.LOG_SOURCE, $"Game Data Loaded");
     }
 
-    //Debug
-
-    [Header("Debug")]
-
-    [Tooltip("Shows control objects")]
-    public bool DebugShowControlObjects;
-
-    [Tooltip("Shows control objects full path")]
-    public bool DebugShowControlObjectsFullPath;
-
-    [Tooltip("For how long to show the game objects")]
-    public float DebugShowControlObjectsTime = 10f;
-
-
     private void SaveGameData()
     {
         Log.Instance.Info(GameController.LOG_SOURCE, $"Game Data Saving");
