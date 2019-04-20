727daf0d698c224cfbaed0060e9cb95acee36895
Dmitri Kuznetsov <kna_int@mail.ru>
2019-04-20 21:50:49 +0300

    Time Scale updated added sample to show current time scale for affectors

---
+++ Assets/Scripts/Prototype/UpdateTextFromTimeScale.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class UpdateTextFromTimeScale : MonoBehaviour
+{
+    [Tooltip("Current Time Scale Instance assigned for this object")]
+    [SerializeField]
+    private TimeControlTimeScale TimeScaleInstance;
+
+    //Control to update text for
+    private Text _text;
+
+    void Start()
+    {
+        _text = gameObject.GetComponent<Text>();
+        TimeScaleInstance = TimeControlController.Instance.CreateTimeScaleInstance(this);
+    }
+
+    void Update()
+    {
+        TimeScaleInstance.Update();
+
+        _text.text = $"{TimeScaleInstance.AffectionName} : {TimeScaleInstance.AffectionTimeScale.ToString("0.00")} = {TimeScaleInstance.CurrentTime.ToString("0.00")} - {TimeScaleInstance.TimeScaleDelta.ToString("0.00")} Delta";
+    }
+}
